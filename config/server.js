e7fd2872ae19a3abcdd07f18c03181d32dace4ff
baldegit <elhibrbalde@gmail.com>
2020-08-06 23:37:43 +0200

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '80cb0b79358d6172816454c16a059acb'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '38d364c5-73dd-42af-ac47-d85c8496fea5'
+};
